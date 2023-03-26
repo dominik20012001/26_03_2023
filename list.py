e712d422d9ee6798f3ca1d9590b7b0cac8086e6f
dominik20012001 <39772120+dominik20012001@users.noreply.github.com>
2023-03-26 12:05:28 +0200

    Add files via upload

---
+++ list.py
@@ -0,0 +1,21 @@
+# x = True
+# if bool(x):
+#     print('hura jrst')
+# else:
+#     print('ni no')
+# print(dir(list))
+
+# def funkcja_2(x, y=10):
+#     print(f'pierwszy argument to {x}, a druga to {y}')
+
+# funkcja_2('|aaa|',11)
+
+# lst = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# for num in lst:
+#     if num % 2 == 0:
+#         print(num)
+
+lst = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for num in lst:
+    if num % 2 != 0:
+        print(num)
