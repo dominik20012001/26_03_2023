acdba74f8b1ca85ec87f1c61883231598105b036
dominik20012001 <39772120+dominik20012001@users.noreply.github.com>
2023-03-26 13:58:50 +0200

    Add files via upload

---
+++ tupla.py
@@ -0,0 +1,19 @@
+# ingredients = ['sól', 'woda', 'mąka']
+
+# print(list(map(lambda i: i.title(), ingredients)))
+# print(list(map(lambda ingr: (ingr, len(ingr)), ingredients)))
+
+# miasta = ('Rybnik','Katowice','Gliwice','Racibórz')
+# print(list(map(lambda s: s[:3], miasta)))
+
+
+# def funkcja_funkcja(a , b , *c):
+#     print 
+#     ...
+
+
+x=int(input)
+y=int(input)
+x + y / 
+ 
+
